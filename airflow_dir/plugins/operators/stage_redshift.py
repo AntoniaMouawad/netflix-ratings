80d77d0210f8488ae38c767cba5ae96e521e61b0
Antonia Mouawad <antoniam@scribd.com>
2021-08-02 23:03:26 -0400

    Update requirements

---
+++ airflow_dir/plugins/operators/stage_redshift.py
@@ -0,0 +1,38 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.models import BaseOperator
+from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
+
+
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
+    template_fields = ("s3_key",)
+
+    def __init__(self,
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 s3_bucket="",
+                 s3_key="",
+                 table="",
+                 *args, **kwargs):
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.table = table
+
+    def execute(self, context):
+        aws_hook = AwsBaseHook(self.aws_credentials_id)
+        aws_credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info(f"Clearing data from destination Redshift table: {self.table}")
+        redshift.run(f"DELETE FROM {self.table}")
+
+        self.log.info("Copying data from S3 to Redshift")
+
+        rendered_key = self.s3_key.format(**context)
+        s3_path = f"s3://{self.s3_bucket}/{rendered_key}"
+
+        redshift.run(f"COPY {self.table} FROM '{s3_path}' ACCESS_KEY_ID '{aws_credentials.access_key}' \
+            SECRET_ACCESS_KEY '{aws_credentials.secret_key}' delimiter '\t' CSV HEADER'")
